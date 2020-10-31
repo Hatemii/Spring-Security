481e8f425cb059c849fdae988ca2ea57e6aea03f
Hatemii <hatem.xhezairi96@gmail.com>
2020-10-31 12:01:00 +0100

    basic auth - postman test

--- src/main/java/com/example/demo/Student/StudentController.java
+++ src/main/java/com/example/demo/Student/StudentController.java
@@ -1,13 +1,8 @@
 package com.example.demo.Student;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
-import java.util.Optional.*;
 
 @RestController
 @RequestMapping("api/students")
