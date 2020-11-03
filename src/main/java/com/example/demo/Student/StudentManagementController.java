a44ba6aa3fdf04203ccf7f8d91f68f500709c788
Hatemii <hatem.xhezairi96@gmail.com>
2020-11-03 23:05:25 +0100

    Adding .authorities to Users with preAuthorize - SECOND WAY

--- src/main/java/com/example/demo/Student/StudentManagementController.java
+++ src/main/java/com/example/demo/Student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.example.demo.Student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -17,26 +18,44 @@ public class StudentManagementController {
 
     // management
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents() {
         return STUDENTS;
     }
 
     // insert
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void insertStudent(@RequestBody Student student) {
         System.out.println(student);
     }
 
     // update
     @PutMapping(value = "/{id}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("id") int id, @RequestBody Student student) {
         System.out.printf(String.format("%s %s", id, student));
     }
 
     // delete
     @DeleteMapping(value = "/{id}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("id") int id) {
         System.out.println(id);
     }
 
 }
+
+/**
+ * @PreAuthorize annotation is used on a method level.
+ * ..
+ * 1. When using the hasRole() security expression, the prefix ROLE_ is skipped.
+ * This is because Spring Framework will add the prefix automatically for us.
+ * ..
+ * 2. When you need to support multiple roles, you can use the hasAnyRole() expression.
+ * ..
+ * 3. @PreAuthorize annotation can also be used with hasAuthority(). When using hasAuthority() expression,
+ * you will need to provide a complete authority name.
+ * ..
+ * 4. When you need to support multiple authorities, hasAnyAuthority() expression can be used.
+ */
